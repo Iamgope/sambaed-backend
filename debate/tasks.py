4311498bfbea725873bd81a4ea111f34a6856990
Razi Ahmad <mail2razi4u@gmail.com>
2026-04-28 15:03:12 +0530

    Remove sample debug task from Celery tasks in debate module

--- debate/tasks.py
+++ debate/tasks.py
@@ -3,12 +3,6 @@ from channels.layers import get_channel_layer
 
 
 @shared_task
-def debug_ping() -> str:
-    """Smoke-test task; replace or remove when real tasks are added."""
-    return "pong"
-
-
-@shared_task
 def send_advance_round_event(group_name: str, data: dict) -> None:
     channel_layer = get_channel_layer()
     channel_layer.group_send(
