cf673a865fd6094700437d69f48af1bc0f4036d2
Razi Ahmad <mail2razi4u@gmail.com>
2026-05-16 14:39:50 +0530

    Add TopicNews admin interface and clean up imports
    - Registered the TopicNews model in the Django admin with a custom ModelAdmin for better management.
    - Defined fields, list filters, and search capabilities for the TopicNews admin interface.
    - Removed unused imports in views.py to streamline the codebase.
    - Added new utility files for future enhancements in the news app.

--- news/admin.py
+++ news/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from unfold.admin import ModelAdmin
+from news.models import TopicNews
+
 
 # Register your models here.
+@admin.register(TopicNews)
+class TopicNewsAdmin(ModelAdmin):
+    fields = ("topic", "content", "pro_content", "con_content")
+    list_filter = ("topic",)
+    search_fields = ("topic",)
--- news/views.py
+++ news/views.py
@@ -3,10 +3,8 @@ from rest_framework.permissions import IsAuthenticated
 from rest_framework_simplejwt.authentication import JWTAuthentication
 
 from base.decorators import handle_exception
-from base.exception import ServiceException
-from base.response import status_200, status_400
+from base.response import status_200
 
-from news.selectors import get_topic_news_by_topic_id
 from news.serializers import TopicNewsSerializer
 from news.services import get_topic_news_data_by_topic_id
 
